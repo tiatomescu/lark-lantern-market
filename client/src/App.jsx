da3760ce3488fbe6e0a10297aba35fcbb89934d8
tiatomescu <tia.yt.tomescu@gmail.com>
2025-04-01 11:05:31 -0500

    Basic fullstack (client/server) framework created!

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>
+        My React App
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ server/knexfile.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: connectionString,
+    }
+};
